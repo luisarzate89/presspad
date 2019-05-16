c3f2b331069265bda3ea1b76947c30a6cf0422f4
ramy <ramyshurafa@hotmail.com>
2019-05-16 11:56:20 +0300

    feat: create header common component Relates #52 #12

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,11 +1,22 @@
 import React, { Component } from "react";
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
+// Antd style
+import "antd/lib/layout/style/index.css";
+import "antd/lib/menu/style/index.css";
+import "antd/lib/breadcrumb/style/index.css";
+
+import Header from "./Common/Header";
 class App extends Component {
   render() {
     return (
       <Router>
-        <div className="App">App</div>
+        <>
+          <Header isLoggedin />
+          <Switch>
+            <div className="App">App</div>
+          </Switch>
+        </>
       </Router>
     );
   }
---
+++ client/src/components/Common/Header/Header.style.js
@@ -0,0 +1,40 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  position: fixed;
+  width: 100%;
+  height: 60px;
+  background-color: #07294a;
+  color: #ffffff;
+`;
+
+export const Menu = styled.ul`
+  width: 80%;
+  margin: 0 auto;
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  height: 100%;
+`;
+
+export const Item = styled.li`
+  list-style: none;
+  margin-left: ${props => (props.logo ? 0 : "50px")};
+  font-family: Roboto;
+  font-weight: 500;
+  font-size: 20px;
+  margin-right: ${props => (props.logo ? "auto" : "initial")};
+
+  a {
+    color: inherit;
+    text-decoration: none;
+  }
+  .active {
+    font-weight: bold;
+    font-size: 20px;
+  }
+`;
+
+export const Logo = styled.img`
+  max-width: 136px;
+`;
