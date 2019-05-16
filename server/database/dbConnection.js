3e0961efa59b0b04a0afe92b9325c71c8fcdc348
Simon Dupree <dupreenator@gmail.com>
2019-05-16 10:32:54 +0200

    inserted user model

--- server/database/dbConnection.js
+++ server/database/dbConnection.js
@@ -1,20 +1,23 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 // read the config file
-// require("dotenv").config();
-require("env2")("./.env");
+require('dotenv').config();
 
 let mongoURI = process.env.MONGO_URI;
 
-if (process.env.NODE_ENV === "test") {
+if (process.env.NODE_ENV === 'test') {
   // change mongoURI to testing database URI
   mongoURI = process.env.MONGOURI_TEST;
+} else if (process.env.NODE_ENV !== 'production') {
+  // change mongoURI to testing database URI
+  mongoURI = process.env.MONGO_URI_PROD;
 }
-
+// mongodb://localhost:27017/connect5db
 // create DB connection
-const dbConnection = () => mongoose.connect(mongoURI, {
-  useNewUrlParser: true,
-  useCreateIndex: true,
-});
+const dbConnection = () =>
+  mongoose.connect(mongoURI, {
+    useNewUrlParser: true,
+    useCreateIndex: true
+  });
 
 module.exports = dbConnection;
---
+++ server/database/models/User.js
@@ -0,0 +1,60 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const { Schema, model } = mongoose;
+
+const userSchema = new Schema({
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+    trim: true,
+    lowercase: true
+  },
+  name: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  // the trainer's local lead
+  referalCode: {
+    type: Schema.Types.ObjectId,
+    ref: 'refCodes'
+  },
+  organisation: {
+    type: Schema.Types.ObjectId,
+    ref: 'organisations'
+  },
+  role: {
+    type: String,
+    enum: ['admin', 'superhost', 'host', 'organisation', 'intern'],
+    required: true
+  }
+});
+
+userSchema.pre('save', async function hashPasswrod() {
+  if (this.isNew || this.isModified('password')) {
+    const document = this;
+    try {
+      const hashedPassword = await bcrypt.hash(document.password, 8);
+      document.password = hashedPassword;
+    } catch (err) {
+      throw new Error('Something bad happened');
+    }
+  } else {
+    throw new Error('Invalid data');
+  }
+});
+
+// check password method
+userSchema.methods.isCorrectPassword = function isCorrectPassword(password) {
+  return bcrypt.compare(password, this.password);
+};
+
+const User = model('users', userSchema);
+
+module.exports = User;
