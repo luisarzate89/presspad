28c1ce4eef98f81a03ab41607d2ff4208eb09368
Simon Dupree <dupreenator@gmail.com>
2019-05-22 15:15:53 +0200

    added profile header section

--- client/src/components/Pages/HostProfile/Profile.style.js
+++ client/src/components/Pages/HostProfile/Profile.style.js
@@ -11,30 +11,81 @@ export const Wrapper = styled.div`
     text-align: center;
     margin-left: auto;
     margin-right: auto;
-    border: 1px solid;
   }
 `;
 
 export const LinkDiv = styled.div`
    {
-    height: 70px;
+    margin-top: 15px;
+    height: 25px;
   }
 `;
 export const BackLinkDiv = styled.div`
    {
+    margin-left: -10px;
     display: flex;
     justify-content: flex-start;
-    border: 1px solid red;
   }
 `;
 
 export const Arrow = styled(BackArrowIcon)`
-  height: 1rem;
-  width: 7vw;
+  width: 4vw;
 `;
 
 export const BackLink = styled(Link)`
   color: ${colors.links};
   text-decoration: none;
   font-weight: 500;
+  line-height: 1;
+`;
+
+export const Header = styled.div`
+  margin-top: 15px;
+  border: 1px solid blue;
+  display: flex;
+`;
+
+export const ProfilePicDiv = styled.div`
+  width: 90px;
+  height: 90px;
+  border-radius: 50%;
+  background-repeat: no-repeat;
+  background-position: center center;
+  background-size: cover;
+  background-image: url(${({ src }) => src});
+  filter: blur(2px);
+  -webkit-filter: blur(2px);
+`;
+
+export const HeadlineDiv = styled.div`
+  height: 90px;
+  width: 75%;
+  display: flex;
+  flex-direction: column;
+  margin-left: 25px;
+  text-align: left;
+`;
+
+export const Headline = styled.h1`
+  font-weight: 900;
+  font-size: 28px;
+`;
+
+export const Address = styled.h3`
+  font-size: 16px;
+  margin-top: auto;
+`;
+
+export const SymbolDiv = styled.div`
+  position: relative;
+  width: 25%;
+`;
+
+export const Symbol = styled.div`
+  width: 38px;
+  height: 50px;
+  background-image: url(${({ src }) => src});
+  right: 0;
+  bottom: 0;
+  position: absolute;
 `;
--- client/src/components/Pages/HostProfile/index.js
+++ client/src/components/Pages/HostProfile/index.js
@@ -5,8 +5,19 @@ import {
   LinkDiv,
   BackLinkDiv,
   Arrow,
-  BackLink
+  BackLink,
+  Header,
+  ProfilePicDiv,
+  HeadlineDiv,
+  Headline,
+  Address,
+  SymbolDiv,
+  Symbol
 } from "./Profile.style";
+
+// images
+import adamProfile from "./../../../assets/profile-pictures/adam-profile.jpeg";
+import starSign from "./../../../assets/star-sign-symbol.svg";
 
 class HostProfile extends Component {
   render() {
@@ -18,6 +29,16 @@ class HostProfile extends Component {
             <BackLink to="/">back to search results</BackLink>
           </BackLinkDiv>
         </LinkDiv>
+        <Header>
+          <ProfilePicDiv src={adamProfile} />
+          <HeadlineDiv>
+            <Headline>A policy research editor at Financial Times</Headline>
+            <Address>12 Marylbone St., London</Address>
+          </HeadlineDiv>
+          <SymbolDiv>
+            <Symbol src={starSign} />
+          </SymbolDiv>
+        </Header>
       </Wrapper>
     );
   }
