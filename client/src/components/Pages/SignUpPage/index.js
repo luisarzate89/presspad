759b7522be0a315796d1a7502afca27b985ae62f
Joe Friel <joseph.s.friel@gmail.com>
2019-05-22 18:48:08 +0100

    Add client side sign up form validation > #23

--- client/src/components/Pages/SignUpPage/index.js
+++ client/src/components/Pages/SignUpPage/index.js
@@ -34,16 +34,125 @@ export default class SignUpPage extends Component {
     this.setState({
       fields
     });
+
+    if (["password", "password2"].includes(e.target.name))
+      this.passwordValidation();
+  };
+
+  passwordValidation = e => {
+    const { fields } = this.state;
+    const errors = {};
+    let formIsValid = true;
+
+    // regex for password validation
+    const pattern = new RegExp(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}$/g);
+
+    if (!pattern.test(fields.password)) {
+      errors.passwordError =
+        "* Password must include at least 8 characters, 1 Uppercase character and 1 number";
+      formIsValid = false;
+    } else if (fields.password !== fields.password2) {
+      errors.password2Error = "* Passwords do not match";
+      formIsValid = false;
+    }
+
+    this.setState({
+      errors
+    });
+
+    return formIsValid;
+  };
+
+  validateForm = () => {
+    const { fields } = this.state;
+    const errors = {};
+    let formIsValid = true;
+
+    if (!fields.name) {
+      formIsValid = false;
+      errors.nameError = "* Please enter your name";
+    }
+
+    if (!fields.email) {
+      formIsValid = false;
+      errors.emailError = "* Please enter your email";
+    }
+
+    if (!fields.orgCode) {
+      formIsValid = false;
+      errors.orgCodeError = "* Please enter your unique code";
+    }
+
+    if (typeof fields.email !== "undefined") {
+      // regular expression for email validation
+      const pattern = new RegExp(
+        /^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)/i
+      );
+      if (!pattern.test(fields.email)) {
+        formIsValid = false;
+        errors.emailError = "* Please enter valid email.";
+      }
+    }
+
+    // regex for password validation
+    const passwordPattern = new RegExp(
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}$/g
+    );
+
+    if (!passwordPattern.test(fields.password)) {
+      errors.passwordError =
+        "* Password must include at least 8 characters, 1 Uppercase character and 1 number";
+      formIsValid = false;
+    }
+
+    if (fields.password !== fields.password2) {
+      errors.password2Error = "* Passwords do not match";
+      formIsValid = false;
+    }
+
+    this.setState({
+      errors
+    });
+
+    return formIsValid;
+  };
+
+  onFormSubmit = e => {
+    const { fields } = this.state;
+    e.preventDefault();
+    const isValid = this.validateForm();
+    if (isValid) {
+      console.log("SUCCESS");
+      // const { email, password } = fields;
+      // const loginData = { email, password };
+      // axios
+      //   .post(API_LOGIN_URL, loginData)
+      //   .then(({ data }) => {
+      //     this.props.handleChangeState({ ...data, isLoggedIn: true });
+      //     ["admin", "organisation"].includes(data.role)
+      //       ? this.props.history.push(DASHBOARD_URL)
+      //       : this.props.history.push(MYPROFILE_URL);
+      //   })
+      //   .catch(err => {
+      //     this.setState({ msg: "error" });
+      //   });
+    } else {
+      console.log("NOOO");
+    }
   };
 
   render() {
     const { fields, errors, msg } = this.state;
     const { email, password, password2, name, orgCode } = fields;
-    const { nameError, emailError, passwordError, password2Error } = errors;
-    const { onInputChange } = this;
+    const {
+      nameError,
+      emailError,
+      orgCodeError,
+      passwordError,
+      password2Error
+    } = errors;
+    const { onInputChange, onFormSubmit } = this;
     const { userType } = this.props;
-    console.log(userType);
-    console.log(USER_TYPES);
     return (
       <Wrapper>
         <HeaderText>
@@ -94,7 +203,7 @@ export default class SignUpPage extends Component {
                 onChange={onInputChange}
                 value={orgCode}
               />
-              <ErrorMsg>{emailError}</ErrorMsg>
+              <ErrorMsg>{orgCodeError}</ErrorMsg>
             </InputDiv>
           )}
           <InputDiv>
@@ -103,7 +212,7 @@ export default class SignUpPage extends Component {
               placeholder="Enter your password"
               name="password"
               id="password"
-              type="text"
+              type="password"
               size="large"
               onChange={onInputChange}
               value={password}
@@ -114,9 +223,9 @@ export default class SignUpPage extends Component {
             <InputLabel htmlFor="password2">Confirm Password</InputLabel>
             <Input.Password
               placeholder="Re-enter your password"
-              name="password"
+              name="password2"
               id="password2"
-              type="text"
+              type="password"
               size="large"
               onChange={onInputChange}
               value={password2}
@@ -124,7 +233,7 @@ export default class SignUpPage extends Component {
             <ErrorMsg>{password2Error}</ErrorMsg>
           </InputDiv>
           <ButtonWrapper>
-            <Button label="Sign in" type="primary" />
+            <Button label="Sign in" type="primary" onClick={onFormSubmit} />
           </ButtonWrapper>
 
           <ErrorMsg>{msg}</ErrorMsg>
