2c660221bb8c11fa9fbef6a7432e82bedcf3b782
Simon Dupree <dupreenator@gmail.com>
2019-05-23 10:49:26 +0200

    test update

--- server/__test__/database/queries/hostProfile.js
+++ server/__test__/database/queries/hostProfile.js
@@ -18,10 +18,21 @@ describe("Tests for hostProfile queries", () => {
     mongoose.disconnect();
   });
 
-  test("Test query", async (done) => {
+  test("Test query with valid id", async (done) => {
     const hosts = await User.find({ role: "host" });
     await hostProfileData(hosts[0]._id).then((profileData) => {
-      expect(profileData).toBe(0);
+      expect(profileData).toBeDefined();
+      expect(profileData[0].email).toBe(hosts[0].email);
+      expect(profileData[0].listing._id).toBeDefined();
+      expect(profileData[0].profile._id).toBeDefined();
+      expect(profileData[0].reviews[0]._id).toBeDefined();
+    });
+    done();
+  });
+
+  test("Test query with invalid id", async (done) => {
+    await hostProfileData("123456").catch((err) => {
+      expect(err).toBeDefined();
     });
     done();
   });
