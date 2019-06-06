aca98c623a726a9d8db9e772389552347529cba2
Joe Friel <joseph.s.friel@gmail.com>
2019-06-06 12:42:51 +0100

    Add query to search listings based on dates and city > #25

--- server/__test__/database/queries/profile.searchProfiles.js
+++ server/__test__/database/queries/profile.searchProfiles.js
@@ -34,4 +34,66 @@ describe("Tests for hostProfile queries", () => {
     });
     done();
   });
+
+  test("Get listings when only date entered and available dates selected", async (done) => {
+    const data = { startDate: "2020-02-12", endDate: "2020-02-15" };
+
+    await searchProfiles(data).then((results) => {
+      expect(results).toBeDefined();
+      expect(results.length).toBe(2);
+    });
+    done();
+  });
+
+  test("Get no listings when only date entered and unavailable dates selected", async (done) => {
+    const data = { startDate: "2020-12-13", endDate: "2020-12-15" };
+
+    await searchProfiles(data).then((results) => {
+      expect(results).toBeDefined();
+      expect(results.length).toBe(0);
+    });
+    done();
+  });
+
+  test("Get listings with city and dates", async (done) => {
+    const data = { city: "London", startDate: "2020-02-12", endDate: "2020-02-15" };
+
+    await searchProfiles(data).then((results) => {
+      expect(results).toBeDefined();
+      expect(results.length).toBe(2);
+      expect(results[0].address.city).toBe("London");
+      expect(results[1].address.city).toBe("London");
+    });
+    done();
+  });
+
+  test("Get no listings with city and dates if city not found", async (done) => {
+    const data = { city: "Birmingham", startDate: "2020-02-12", endDate: "2020-02-15" };
+
+    await searchProfiles(data).then((results) => {
+      expect(results).toBeDefined();
+      expect(results.length).toBe(0);
+    });
+    done();
+  });
+
+  test("Get no listings with city and dates if dates not found", async (done) => {
+    const data = { city: "London", startDate: "2020-12-13", endDate: "2020-12-15" };
+
+    await searchProfiles(data).then((results) => {
+      expect(results).toBeDefined();
+      expect(results.length).toBe(0);
+    });
+    done();
+  });
+
+  test("Get no listings with city and dates if dates and city not found", async (done) => {
+    const data = { city: "Birmingham", startDate: "2020-12-13", endDate: "2020-12-15" };
+
+    await searchProfiles(data).then((results) => {
+      expect(results).toBeDefined();
+      expect(results.length).toBe(0);
+    });
+    done();
+  });
 });
--- server/database/queries/profile/searchProfiles.js
+++ server/database/queries/profile/searchProfiles.js
@@ -11,4 +11,48 @@ module.exports.searchProfiles = searchInfo => new Promise((resolve, reject) => {
       .then(listings => resolve(listings))
       .catch(error => reject(error));
   }
+
+  // if only dates get all listing that match those dates
+  else if (!city) {
+    Listing.aggregate([
+      // get any listings that have an end date later than today
+      {
+        $match: { "availableDates.endDate": { $gte: new Date() } },
+      },
+      // get any listings that have an end date later then the user's start date
+      {
+        $match: { "availableDates.endDate": { $gte: new Date(startDate) } },
+      },
+      // then get listings that have a start date before the user's end date
+      {
+        $match: { "availableDates.startDate": { $lt: new Date(endDate) } },
+      },
+    ])
+      .then(listings => resolve(listings))
+      .catch(error => reject(error));
+  }
+
+  // otherwise find those that match city AND dates
+  else {
+    Listing.aggregate([
+      // get any listings that match the city
+      {
+        $match: { "address.city": city },
+      },
+      // get any listings that have an end date later than today
+      {
+        $match: { "availableDates.endDate": { $gte: new Date() } },
+      },
+      // get any listings that have an end date later then the user's start date
+      {
+        $match: { "availableDates.endDate": { $gte: new Date(startDate) } },
+      },
+      // then get listings that have a start date before the user's end date
+      {
+        $match: { "availableDates.startDate": { $lt: new Date(endDate) } },
+      },
+    ])
+      .then(listings => resolve(listings))
+      .catch(error => reject(error));
+  }
 });
