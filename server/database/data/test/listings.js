613363f66f07c15520c6bf838c22c5d08239d6e3
ramy <ramyshurafa@hotmail.com>
2019-07-27 22:09:12 +0300

    test: fix faild tests

--- server/database/data/test/listings.js
+++ server/database/data/test/listings.js
@@ -18,9 +18,18 @@ module.exports = async () => {
       otherInfo: ["Pets allowed", "No other flatmates", "LGBTQ friendly", "Often away"],
       price: 30,
       photos: [
-        "https://i.ibb.co/rbBsRvD/1livingroom.jpg",
-        "https://i.ibb.co/hXFFfNz/1kitchen.jpg",
-        "https://i.ibb.co/VvXFwg4/1bathroom.jpg",
+        {
+          fileName: "test-image-1.png",
+          isPrivate: false,
+        },
+        {
+          fileName: "test-image-2.png",
+          isPrivate: false,
+        },
+        {
+          fileName: "test-image-3.png",
+          isPrivate: false,
+        },
       ],
       availableDates: [
         { startDate: "2019-05-16", endDate: "2019-07-16" },
