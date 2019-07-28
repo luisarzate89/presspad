8f5d389076ad7747642c028201d625ec41fa972b
FarahZaqout <farah.zaqout@gmail.com>
2019-07-28 14:18:54 -0700

    update review controller with proper responses
    #113

--- server/controllers/user/review.js
+++ server/controllers/user/review.js
@@ -1,14 +1,33 @@
 const boom = require("boom");
 const { createReview } = require("../../database/queries/reviews");
+const { createNotification } = require("../../database/queries/notifications");
 
-const reviewControllers
-module.exports = {}
+const reviewControllers = {};
+module.exports = reviewControllers;
 
-reviewControllers.gerReview = (req, res, next) => {
-  const { to, from, rating, message } = req.body
+reviewControllers.createReview = async (req, res, next) => {
+  const {
+    to, from, rating, message,
+  } = req.body;
+
+  // VALIDATES USER INPUT
+  if (!rating) return next(boom.badRequest("Please pick the number of stars"));
+  if (!message) return next(boom.badRequest("Please add a review message"));
   try {
-    await createReview({ to, from, rating, message })
-  } catch(error) {
-    next(error)
+    // create a review
+    await createReview({
+      to, from, rating, message,
+    });
+
+    // send a notification to the reviewee
+    await createNotification({
+      user: to,
+      secondParty: from,
+      type: "getReview",
+    });
+
+    res.json({ success: true });
+  } catch (error) {
+    next(boom.badImplementation());
   }
-}
+};
