87e8c0ca0b13ce6be29f73f9d6b200eb75b9e69f
FarahZaqout <farah.zaqout@gmail.com>
2019-07-30 09:50:09 -0700

    modifies tests to make sure authentication works on the review route #113

--- server/__test__/api/reviews.js
+++ server/__test__/api/reviews.js
@@ -19,6 +19,11 @@ describe("Tests adding a review and creating a getReview notification", () => {
     await mongoose.disconnect();
   });
 
+  const loginData = {
+    email: "josephine@guardian.co.uk",
+    password: "123456",
+  };
+
   test("tests adding a review successfully", async (done) => {
     const reviewee = await User.findOne({ name: "Michael Peters" });
     const reviewer = await User.findOne({ name: "Josephine Doeski" });
@@ -30,33 +35,45 @@ describe("Tests adding a review and creating a getReview notification", () => {
       message: "Farah cannot use jest",
     };
 
-    // Request should create a document in Review collection and a document in Notification collection.
     request(app)
-      .post(`/api/users/${reviewer.id}/reviews`)
-      .send(reviewData)
-      .expect(200)
+      .post("/api/user/login")
+      .send(loginData)
       .expect("Content-Type", /json/)
-      .end(async (err, res) => {
-        if (err) return done(err);
-        // make sure a res.end() is called somewhere in the controller
-        expect(res).toBeDefined();
+      .expect(200)
+      .end((error, response) => {
+        const token = response.headers["set-cookie"][0].split(";")[0];
 
-        // find the newly created documents (in Review and Notification schema)
-        const review = await Review.findOne({ to: reviewData.to });
-        const notification = await Notification.findOne({ secondParty: reviewData.from });
+        // Request should create a document in Review collection
+        // and a document in Notification collection.
+        request(app)
+          .post(`/api/users/${reviewer.id}/reviews`)
+          .send(reviewData)
+          .set("Cookie", [token])
+          .expect(200)
+          .expect("Content-Type", /json/)
+          .end(async (err, res) => {
+            if (err) return done(err);
+            // make sure a res.end() is called somewhere in the controller
+            expect(res).toBeDefined();
 
-        // there should be a review and a notification entries.
-        // fails if returned value is null
-        expect(review).toBeTruthy();
-        expect(notification).toBeTruthy();
+            // find the newly created documents (in Review and Notification schema)
+            const review = await Review.findOne({ to: reviewData.to });
+            const notification = await Notification.findOne({ secondParty: reviewData.from });
 
-        // the new entry should have a type of getReview
-        expect(notification.type).toBe("getReview");
-        // the two documents should be linked via ids from the user schema
-        expect(notification.user).toMatchObject(review.to);
-        expect(notification.secondParty).toMatchObject(review.from);
-        return done();
+            // there should be a review and a notification entries.
+            // fails if returned value is null
+            expect(review).toBeTruthy();
+            expect(notification).toBeTruthy();
+
+            // the new entry should have a type of getReview
+            expect(notification.type).toBe("getReview");
+            // the two documents should be linked via ids from the user schema
+            expect(notification.user).toMatchObject(review.to);
+            expect(notification.secondParty).toMatchObject(review.from);
+            return done();
+          });
       });
+
   }, 30000);
 
   test("check that input validation works as intended for rating", async (done) => {
@@ -70,15 +87,25 @@ describe("Tests adding a review and creating a getReview notification", () => {
       message: "Farah cannot use jest",
     };
 
-    // Request should fail with a bad request error (code 400)
     request(app)
-      .post(`/api/users/${reviewer.id}/reviews`)
-      .send(reviewData)
-      .expect(400)
+      .post("/api/user/login")
+      .send(loginData)
       .expect("Content-Type", /json/)
-      .end((err) => {
-        if (err) return done(err);
-        return done();
+      .expect(200)
+      .end((error, response) => {
+        const token = response.headers["set-cookie"][0].split(";")[0];
+
+        // Request should fail with a bad request error (code 400)
+        request(app)
+          .post(`/api/users/${reviewer.id}/reviews`)
+          .send(reviewData)
+          .send("Cookie", [token])
+          .expect(400)
+          .expect("Content-Type", /json/)
+          .end((err) => {
+            if (err) return done(err);
+            return done();
+          });
       });
   }, 30000);
 
@@ -93,16 +120,25 @@ describe("Tests adding a review and creating a getReview notification", () => {
       rating: 4,
       message: "",
     };
-
-    // Request should fail with a bad request error (code 400)
     request(app)
-      .post(`/api/users/${reviewer.id}/reviews`)
-      .send(reviewData)
-      .expect(400)
+      .post("/api/user/login")
+      .send(loginData)
       .expect("Content-Type", /json/)
-      .end((err) => {
-        if (err) return done(err);
-        return done();
+      .expect(200)
+      .end((error, response) => {
+        const token = response.headers["set-cookie"][0].split(";")[0];
+
+        // Request should fail with a bad request error (code 400)
+        request(app)
+          .post(`/api/users/${reviewer.id}/reviews`)
+          .send(reviewData)
+          .set("Cookie", [token])
+          .expect(400)
+          .expect("Content-Type", /json/)
+          .end((err) => {
+            if (err) return done(err);
+            return done();
+          });
       });
   }, 30000);
 });
