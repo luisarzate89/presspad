0ed0a5f733ac8995e8cd042bdbdbe74f8727edc3
FarahZaqout <farah.zaqout@gmail.com>
2019-07-30 01:59:13 -0700

    adds tests for the reviews controller testing successful document creation testing validation for user input for bot rating and message #113

--- server/__test__/api/reviews.js
+++ server/__test__/api/reviews.js
@@ -24,25 +24,85 @@ describe("Tests adding a review and creating a getReview notification", () => {
     const reviewer = await User.findOne({ name: "Josephine Doeski" });
 
     const reviewData = {
-      to: reviewee.id, // person receiving the review >> also the second party in notification
-      from: reviewer.id,
+      to: reviewee.id, // person receiving the review >> also the user in notification
+      from: reviewer.id, // person sending the creating, also the secondParty in notification
       rating: 4,
       message: "Farah cannot use jest",
     };
 
+    // Request should create a document in Review collection and a document in Notification collection.
     request(app)
       .post(`/api/users/${reviewer.id}/reviews`)
       .send(reviewData)
       .expect(200)
       .expect("Content-Type", /json/)
       .end(async (err, res) => {
-        if (err) return console.error(err);
+        if (err) return done(err);
+        // make sure a res.end() is called somewhere in the controller
         expect(res).toBeDefined();
+
+        // find the newly created documents (in Review and Notification schema)
         const review = await Review.findOne({ to: reviewData.to });
-        const notification = await Notification.findById({ secondParty: reviewData.to });
-        expect(notification.secondParty).toBe(review.to);
-        console.log(review);
-        done();
+        const notification = await Notification.findOne({ secondParty: reviewData.from });
+
+        // there should be a review and a notification entries.
+        // fails if returned value is null
+        expect(review).toBeTruthy();
+        expect(notification).toBeTruthy();
+
+        // the new entry should have a type of getReview
+        expect(notification.type).toBe("getReview");
+        // the two documents should be linked via ids from the user schema
+        expect(notification.user).toMatchObject(review.to);
+        expect(notification.secondParty).toMatchObject(review.from);
+        return done();
+      });
+  }, 30000);
+
+  test("check that input validation works as intended for rating", async (done) => {
+    const reviewee = await User.findOne({ name: "Michael Peters" });
+    const reviewer = await User.findOne({ name: "Josephine Doeski" });
+
+    const reviewData = {
+      to: reviewee.id, // person receiving the review >> also the user in notification
+      from: reviewer.id, // person sending the creating, also the secondParty in notification
+      rating: "",
+      message: "Farah cannot use jest",
+    };
+
+    // Request should fail with a bad request error (code 400)
+    request(app)
+      .post(`/api/users/${reviewer.id}/reviews`)
+      .send(reviewData)
+      .expect(400)
+      .expect("Content-Type", /json/)
+      .end((err) => {
+        if (err) return done(err);
+        return done();
+      });
+  }, 30000);
+
+
+  test("check that input validation works as intended for message", async (done) => {
+    const reviewee = await User.findOne({ name: "Michael Peters" });
+    const reviewer = await User.findOne({ name: "Josephine Doeski" });
+
+    const reviewData = {
+      to: reviewee.id, // person receiving the review >> also the user in notification
+      from: reviewer.id, // person sending the creating, also the secondParty in notification
+      rating: 4,
+      message: "",
+    };
+
+    // Request should fail with a bad request error (code 400)
+    request(app)
+      .post(`/api/users/${reviewer.id}/reviews`)
+      .send(reviewData)
+      .expect(400)
+      .expect("Content-Type", /json/)
+      .end((err) => {
+        if (err) return done(err);
+        return done();
       });
   }, 30000);
 });
