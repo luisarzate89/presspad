71d350b0e2082226120937f95d0494b6c4afc484
Ahmed Shatat <a.shatat@hotmail.com>
2019-08-14 17:45:15 +0300

    refactor: generate profileImage url
    #160

--- server/controllers/dashboard/internDashboard.js
+++ server/controllers/dashboard/internDashboard.js
@@ -1,6 +1,8 @@
 const boom = require("boom");
 
 const { internDashboard: internDashboardQuery } = require("../../database/queries/dashboard");
+const { getPublicFileUrl } = require("../../helpers/storage");
+const { storageBucket: bucketName } = require("../../config");
 
 const internDashboard = async (req, res, next) => {
   const { _id: internId, role } = req.user;
@@ -8,7 +10,13 @@ const internDashboard = async (req, res, next) => {
     return next(boom.forbidden());
   }
   try {
-    const dashboardData = await internDashboardQuery(internId);
+    const [dashboardData] = await internDashboardQuery(internId);
+    const { profile: [{ profileImage }] } = dashboardData;
+    if (profileImage.name) {
+      dashboardData.profile = { profileImage: getPublicFileUrl(bucketName, profileImage.name) };
+    } else {
+      dashboardData.profile = {};
+    }
     return res.json({ data: dashboardData });
   } catch (err) {
     return next(err);
