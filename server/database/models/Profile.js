e0f7804f0b904e8a8febff9aea02484ef43872a6
MohammedYehia <m-ya@windowslive.com>
2019-08-17 16:51:22 +0300

    edit userId to be unique in Profile model

--- server/database/models/Profile.js
+++ server/database/models/Profile.js
@@ -2,75 +2,82 @@ const mongoose = require("mongoose");
 
 const { Schema, model } = mongoose;
 
-const profileSchema = new Schema({
-  user: {
-    type: Schema.Types.ObjectId,
-    ref: "users",
-  },
-  verified: {
-    type: Boolean,
-    default: false,
-  },
-  bio: {
-    type: String,
-    required: true,
-  },
-  interests: String,
-  organisation: {
-    name: String,
-    website: String,
-  },
-  jobTitle: {
-    type: String,
-    lowercase: true,
-    required: true,
-  },
-  profileImage: {
-    fileName: String,
-    isPrivate: {
+const profileSchema = new Schema(
+  {
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: "users",
+      // make surre no duplicate with user id
+      unique: true,
+      // drop any duplicate record
+      dropDups: true,
+    },
+    verified: {
+      type: Boolean,
+      default: false,
+    },
+    bio: {
+      type: String,
+      required: true,
+    },
+    interests: String,
+    organisation: {
+      name: String,
+      website: String,
+    },
+    jobTitle: {
+      type: String,
+      lowercase: true,
+      required: true,
+    },
+    profileImage: {
+      fileName: String,
+      isPrivate: {
+        type: Boolean,
+        default: false,
+      },
+    },
+    favouriteArticle: {
+      title: String,
+      author: String,
+      link: String,
+      description: String,
+    },
+    // later plan would be enum [basic, pro, custom...]
+
+    verification: {
+      photoID: {
+        fileName: String,
+        isPrivate: {
+          type: Boolean,
+          default: true,
+        },
+      },
+      offerLetter: {
+        fileName: String,
+        isPrivate: {
+          type: Boolean,
+          default: true,
+        },
+      },
+      reference1: {
+        name: String,
+        contact: String,
+      },
+      reference2: {
+        name: String,
+        contact: String,
+      },
+    },
+    badge: {
       type: Boolean,
       default: false,
     },
   },
-  favouriteArticle: {
-    title: String,
-    author: String,
-    link: String,
-    description: String,
+  {
+    timestamps: true,
   },
-  // later plan would be enum [basic, pro, custom...]
-
-  verification: {
-    photoID: {
-      fileName: String,
-      isPrivate: {
-        type: Boolean,
-        default: true,
-      },
-    },
-    offerLetter: {
-      fileName: String,
-      isPrivate: {
-        type: Boolean,
-        default: true,
-      },
-    },
-    reference1: {
-      name: String,
-      contact: String,
-    },
-    reference2: {
-      name: String,
-      contact: String,
-    },
-  },
-  badge: {
-    type: Boolean,
-    default: false,
-  },
-}, {
-  timestamps: true,
-});
+);
 
 const Profile = model("profiles", profileSchema);
 
