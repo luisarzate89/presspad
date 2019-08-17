4a55b9ea08dc77a3073fcc2733976074725796d5
FarahZaqout <farah.zaqout@gmail.com>
2019-08-16 18:16:53 -0700

    feat: adds Avatar picture to the AddReview component #99

---
+++ client/src/components/Pages/AddReview/Avatar/AvatarWrapper.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+const AvatarWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  margin-left: 10rem;
+  align-items: center;
+`;
+
+export default AvatarWrapper;
---
+++ client/src/components/Pages/AddReview/Avatar/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Avatar } from "antd";
+import AvatarStyle from "./AvatarStyle";
+import AvatarWrapper from "./AvatarWrapper";
+import Message from "./Message";
+
+const AvatarComponent = () => {
+  return (
+    <AvatarWrapper>
+      <Avatar style={AvatarStyle} />
+      <Message>Hi, Andrew! What do you think of Emily?</Message>
+    </AvatarWrapper>
+  );
+};
+
+export default AvatarComponent;
