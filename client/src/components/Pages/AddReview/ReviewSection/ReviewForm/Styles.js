cd629b310384a51f6e02be15c00006f5457ce1d6
FarahZaqout <farah.zaqout@gmail.com>
2019-08-16 20:34:49 -0700

    adds styles to the review form elements #99

---
+++ client/src/components/Pages/AddReview/ReviewSection/ReviewForm/Styles.js
@@ -0,0 +1,20 @@
+const ButtonStyle = { 
+  width: "35%",
+  "min-width": "150px", 
+  margin: "3rem 0 0 0", 
+  // "background-color": "#0AC7E7",
+  // color: "#fff", 
+  height: "3rem", 
+  "border-radius": "30px",
+};
+
+const CheckboxStyle = { 
+  "font-weight": "650",
+  color: "#000"
+};
+
+const RateStyle = {
+  margin: "0 0 20px 0"
+};
+
+export { RateStyle, CheckboxStyle, ButtonStyle };
