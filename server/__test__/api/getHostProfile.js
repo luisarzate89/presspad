52405c74d781b51de2c17d7a45a698cc43b91c9b
am <abdalsamad.y.m@gmail.com>
2019-08-19 14:49:09 +0300

    fix: update getHostProfile controller to account for not found profiles

--- server/__test__/api/getHostProfile.js
+++ server/__test__/api/getHostProfile.js
@@ -21,35 +21,69 @@ describe("Testing for get host profile route", () => {
     await buildDB();
   });
 
-  test("test with correct user id", async (done) => {
-    const host = await User.findOne({ email: "adam@gmail.com" });
-
+  test("test with correct user id", (done) => {
+    const loginData = {
+      email: "mone@gmail.com",
+      password: "123456",
+    };
     request(app)
-      .get(`/api/host/${host._id}`)
+      .post("/api/user/login")
+      .send(loginData)
       .expect("Content-Type", /json/)
       .expect(200)
-      .end((err, res) => {
-        expect(res).toBeDefined();
-        expect(res.body).toBeDefined();
-        expect(res.body.name).toBe(host.name);
-        expect(res.body.listing).toBeDefined();
-        expect(res.body.profile).toBeDefined();
-        expect(res.body.reviews[0].message).toBeDefined();
-        expect(res.body.reviews[0].from).toBeDefined();
-        done(err);
+      .end(async (error, resp) => {
+        if (error) {
+          done(error);
+          return;
+        }
+        const token = resp.headers["set-cookie"][0].split(";")[0];
+        const host = await User.findOne({ email: "adam@gmail.com" });
+
+        request(app)
+          .get(`/api/host/${host._id}`)
+          .set("Cookie", [token])
+          .expect("Content-Type", /json/)
+          .expect(200)
+          .end((err, res) => {
+            expect(res).toBeDefined();
+            expect(res.body).toBeDefined();
+            expect(res.body.name).toBe(host.name);
+            expect(res.body.listing).toBeDefined();
+            expect(res.body.profile).toBeDefined();
+            expect(res.body.reviews[0].message).toBeDefined();
+            expect(res.body.reviews[0].from).toBeDefined();
+            done(err);
+          });
       });
   });
 
-  test.only("test with incorrect user id", async (done) => {
-    const data = { userId: "5ce66c1635c86b54fd6c732c" };
-
+  test("test with incorrect user id", (done) => {
+    const loginData = {
+      email: "mone@gmail.com",
+      password: "123456",
+    };
     request(app)
-      .get(`/api/host/${data.userId}`)
+      .post("/api/user/login")
+      .send(loginData)
       .expect("Content-Type", /json/)
-      .expect(404)
-      .end((err, res) => {
-        expect(res.body.error).toBeDefined();
-        done(err);
+      .expect(200)
+      .end(async (error, resp) => {
+        if (error) {
+          done(error);
+          return;
+        }
+        const token = resp.headers["set-cookie"][0].split(";")[0];
+        const data = { userId: "5ce66c1635c86b54fd6c732c" };
+
+        request(app)
+          .get(`/api/host/${data.userId}`)
+          .set("Cookie", [token])
+          .expect("Content-Type", /json/)
+          .expect(404)
+          .end((err, res) => {
+            expect(res.body.error).toBeDefined();
+            done(err);
+          });
       });
   });
 });
