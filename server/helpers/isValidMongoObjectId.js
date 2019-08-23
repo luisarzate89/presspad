81e564db5eb69a5cdec1b504db39334ec7142971
amusameh <abdalsamad.y.m@gmail.com>
2019-08-23 09:30:04 +0300

    Add a helper function to check for valid ObjecId

---
+++ server/helpers/isValidMongoObjectId.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+const { ObjectId } = mongoose.Types;
+
+const isValidMongoObjectId = (id) => {
+  if (ObjectId.isValid(id)) {
+    if (ObjectId(id).toString() === id) {
+      return true;
+    }
+    return false;
+  }
+  return false;
+}
+
+module.exports = { isValidMongoObjectId };
