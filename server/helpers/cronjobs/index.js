7f0fca6f8f16c76e3a04cef47791b05cde4136b9
FarahZaqout <farah.zaqout@gmail.com>
2019-08-26 18:56:02 +0300

    adds async keyword to the cronjob function #98

--- server/helpers/cronjobs/index.js
+++ server/helpers/cronjobs/index.js
@@ -2,7 +2,7 @@ const cron = require("node-cron");
 const mailTask = require("./mailCron");
 
 const cronJobs = async () => {
-  return cron.schedule("1 1 3 * * *", () => {
+  return cron.schedule("1 1 3 * * *", async () => {
     await mailTask();
     console.log("hello, cron")
   });
