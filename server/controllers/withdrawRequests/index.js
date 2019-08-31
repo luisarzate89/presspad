d069b1ccaf462c5d8bca60821bf0907d8ebe0369
FarahZaqout <farah.zaqout@gmail.com>
2019-08-31 16:50:24 +0300

    adds validation and tests #178 validation forbids non-admins to view withdrawal requests tests functionality of the feature

--- server/controllers/withdrawRequests/index.js
+++ server/controllers/withdrawRequests/index.js
@@ -3,11 +3,12 @@ const boom = require("boom");
 
 const findWithdrawRequests = async (req, res, next) => {
   try {
+    // only admin is allowed to view all withdrawal requests on presspad.
+    if (req.user.role !== "admin") return res.status(403).json({ message: "Forbidden" });
     const withdrawRequests = await findAllWithdrawRequests();
-    console.log("withdraw requests", withdrawRequests);
     return res.json(withdrawRequests);
   } catch (error) {
-    console.log(error);
+    console.log(error); // for debugging only. Remove before final push.
     next(boom.badImplementation(error));
   };
 };
