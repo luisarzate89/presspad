c5b3683f7f1bb430e7e071468553d1f73f883933
ramy <ramyshurafa@hotmail.com>
2019-09-02 15:27:56 +0300

    show bookings table in intern dashboard relates #132

--- client/src/components/Pages/InternDashboard/BookingsTableSection.js
+++ client/src/components/Pages/InternDashboard/BookingsTableSection.js
@@ -1,20 +1,22 @@
 import React from "react";
 import { Table } from "antd";
 
-import { paymentsColumns } from "./TablesColumns";
+import { bookingsColumns } from "./TablesColumns";
 
 import { SectionTitle } from "./InternDashboard.style";
 import { SectionWrapperContent } from "../../Common/general";
 
-export default function PaymentsSection(props) {
+export default function BookingsSection(props) {
+  const { data, windowWidth } = props;
+
   return (
     <section>
       <SectionWrapperContent style={{ minHeight: 200 }}>
-        <SectionTitle>Your payments</SectionTitle>
+        <SectionTitle>Your Bookings</SectionTitle>
         <Table
-          dataSource={props.data}
-          columns={paymentsColumns}
-          rowKey={record => record._id}
+          dataSource={data}
+          columns={bookingsColumns(windowWidth)}
+          rowKey={"_id"}
         />
       </SectionWrapperContent>
     </section>
