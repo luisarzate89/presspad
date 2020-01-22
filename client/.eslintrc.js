6b65e417ae14887c8c64d5b1b9036e0e8c2af124
Imad Shatali <amoodaa@gmail.com>
2020-01-22 13:20:45 +0200

    edited and disabled rules eslint (#333)
    * new eslint configs
    
    * edited and disabled rules

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -1,60 +1,54 @@
 module.exports = {
-  'extends': [
-    'airbnb',
-    'prettier',
-    'prettier/react'
-  ],
+  extends: ["airbnb", "prettier", "prettier/react"],
   // babel-eslint parser is used to support experimental features not supported in ESLint itself yet
-  'parser': 'babel-eslint',
-  'parserOptions': {
-    'ecmaVersion': 8,
-    'ecmaFeatures': {
-      'impliedStrict': true, //enable global strict mode (if ecmaVersion is 5 or greater)
+  parser: "babel-eslint",
+  parserOptions: {
+    ecmaVersion: 8,
+    ecmaFeatures: {
+      impliedStrict: true //enable global strict mode (if ecmaVersion is 5 or greater)
     }
   },
   root: true,
-  'env': {
-    'browser': true,
-    'node': true,
-    'jest': true
+  env: {
+    browser: true,
+    node: true,
+    jest: true
   },
-  'rules': {
+  rules: {
     "arrow-body-style": ["error", "as-needed"],
-    'react/state-in-constructor': 0,
-    'react/no-spreading-props': 0,
-    'jsx-a11y/click-events-have-key-events': 0,
+    "react/state-in-constructor": 0,
+    "react/jsx-props-no-spreading": 0,
+    "react/prop-types": 0,
+    "no-underscore-dangle": 0,
+    "destructuring-assignment": 0,
+    "dot-notation": 0,
+    "jsx-a11y/click-events-have-key-events": 0,
     // disables the windows/unix linebreak checks.
-    'linebreak-style': 0,
-    'linebreak-style': [0, 'error', 'windows'],
+    "linebreak-style": 0,
+    "linebreak-style": [0, "error", "windows"],
     //  allow .js extensions for JSX.
-    'react/jsx-filename-extension': [
+    "react/jsx-filename-extension": [
       1,
       {
-        'extensions': [
-          '.js',
-          '.jsx'
-        ]
+        extensions: [".js", ".jsx"]
       }
     ],
-    'quotes': [
+    quotes: [
       2,
-      'single',
+      "double",
       {
-        'avoidEscape': true, // allows strings to use single-quotes or double-quotes so long as the string contains a quote that would have to be escaped otherwise
-        'allowTemplateLiterals': true // allows strings to use backticks
+        avoidEscape: true, // allows strings to use single-quotes or double-quotes so long as the string contains a quote that would have to be escaped otherwise
+        allowTemplateLiterals: true // allows strings to use backticks
       }
     ],
     // configure the prettier plugin
-    'prettier/prettier': [
-      'error',
+    "prettier/prettier": [
+      "error",
       {
-        'trailingComma': 'es5',
-        'singleQuote': true,
+        trailingComma: "all",
+        singleQuote: false
       }
     ]
   },
-  'plugins': [
-    'react',
-    'prettier'
-  ]
+  plugins: ["react", "prettier"]
 };
