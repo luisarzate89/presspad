a3a034a859d017a47668906e7df3fca5ed1b4e8c
RamyAlshurafa <34629478+RamyAlshurafa@users.noreply.github.com>
2020-01-25 11:20:13 +0200

    Update the completed profiles validator schema (#360)
    * update completed profiles validator schema
    
    * add the missed 'storyAnswer' question
    
    * Update server/middlewares/validation/internCompleteProfileSchema.js
    
    Co-Authored-By: Ahmed Shatat <a.shatat@hotmail.com>
    
    Co-authored-by: Ahmed Shatat <a.shatat@hotmail.com>

--- server/middlewares/validation/internCompleteProfileSchema.js
+++ server/middlewares/validation/internCompleteProfileSchema.js
@@ -1,37 +1,74 @@
 const Joi = require("@hapi/joi");
 
 const internCompleteProfileSchema = Joi.object({
+  // About you 1 (required for basic profile)
+  birthDate: Joi.date().required(),
+  gender: Joi.string().required(),
+  hometown: Joi.string().required(),
+  school: Joi.string().required(),
   profileImage: Joi.object({
     fileName: Joi.string().required(),
     isPrivate: Joi.boolean().default(false),
   }).required(),
-  verified: Joi.boolean().default(true),
+  interests: Joi.string().required(),
   bio: Joi.string().required(),
-  favouriteArticle: Joi.object({
-    title: Joi.string().required(),
-    author: Joi.string().required(),
-    link: Joi.string().required(),
-    description: Joi.string().required(),
-  }).required(),
-  jobTitle: Joi.string().required(),
-  organisation: Joi.string().allow(""),
+
+  // Other information 1 (required for booking)
+  organisation: Joi.string().required(),
+  useReasonAnswer: Joi.string().required(),
+  issueAnswer: Joi.string().required(),
+  storyAnswer: Joi.string().required(),
+  mentorDescribeAnswer: Joi.string().required(),
+
+  // about you 2 (required for basic profile)
   photoID: Joi.object({
     fileName: Joi.string().required(),
     isPrivate: Joi.boolean().default(true),
   }).required(),
+  hearAboutPressPadAnswer: Joi.string().required(),
+  phoneNumber: Joi.string().required(),
+
+  // Other information 2 (required for booking)
+  reference1: Joi.object({
+    name: Joi.string().required(),
+    email: Joi.string()
+      .email()
+      .required(),
+  }),
+  reference2: Joi.object({
+    name: Joi.string().required(),
+    email: Joi.string()
+      .email()
+      .required(),
+  }),
   offerLetter: Joi.object({
     fileName: Joi.string().required(),
     isPrivate: Joi.boolean().default(true),
   }),
-  reference1: Joi.object({
+  internshipOfficeAddress: Joi.string().required(),
+  emergencyContact: Joi.object({
     name: Joi.string().required(),
-    email: Joi.string().required(),
-  }).required(),
-  reference2: Joi.object({
-    name: Joi.string().required(),
-    email: Joi.string().required(),
-  }).required(),
+    email: Joi.string()
+      .email()
+      .required(),
+    phoneNumber: Joi.string().required(),
+  }),
+  DBSCheck: Joi.object({
+    fileName: Joi.string().required(),
+    isPrivate: Joi.boolean().default(true),
+  }),
 
+  // demographic (required for booking)
+  sexualOrientation: Joi.string().required(),
+  degreeLevel: Joi.string().required(),
+  ethnicity: Joi.string().required(),
+  earningOfParents: Joi.string().required(),
+  disability: Joi.string().required(),
+  parentsWorkInPress: Joi.string().required(),
+  caringResponsibilities: Joi.string().required(),
+  allergies: Joi.string().required(),
+  backgroundAnswer: Joi.string().required(),
+  consentedOnPressPadTerms: Joi.any().allow(true),
 });
 
 module.exports = internCompleteProfileSchema;
