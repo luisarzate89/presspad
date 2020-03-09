de9d8a0c6f2f752abc1bd2662757894746f53e4f
RamyAlshurafa <34629478+RamyAlshurafa@users.noreply.github.com>
2020-03-09 14:40:21 +0200

    fix existing admin tests (#513)

--- server/__test__/api/admin/viewWithdrawRequests.js
+++ server/__test__/api/admin/viewWithdrawRequests.js
@@ -1,84 +1,79 @@
 const request = require('supertest');
-const mongoose = require('mongoose');
 
-const User = require('../../database/models/User');
-const buildDB = require('./../../database/data/test/index');
-const app = require('./../../app');
+const buildDB = require('../../../database/data/test');
+const app = require('../../../app');
+const createToken = require('../../../helpers/createToken');
+
+// API ROUTE
+const {
+  API_FIND_WITHDRAW_REQUESTS_URL,
+} = require('../../../../client/src/constants/apiRoutes');
+
+let connection;
+let users;
+let withdrawRequests;
 
 describe('Testing for get host profile route', () => {
-  beforeAll(async () => {
+  beforeEach(async () => {
     // build dummy data
-    await buildDB();
+    const {
+      connection: _connection,
+      users: _users,
+      withdrawRequests: _withdrawRequests,
+    } = await buildDB();
+    connection = _connection;
+    users = _users;
+    withdrawRequests = _withdrawRequests;
   });
 
   afterAll(async () => {
-    await mongoose.disconnect();
-  });
-
-  beforeEach(async () => {
-    // build dummy data
-    await buildDB();
+    await connection.close();
   });
 
   // tests user validation
   test('Admin is able to view all withdrawal requests', async done => {
-    const admin = await User.findOne({ role: 'admin' });
-    const loginData = {
-      email: admin.email,
-      password: '123456',
-    };
-    // login as admin
+    const { adminUser, hostUser } = users;
+    const { pendingWithdrawRequest } = withdrawRequests;
+
+    const token = `token=${createToken(adminUser._id)}`;
+
     request(app)
-      .post('/api/user/login')
-      .send(loginData)
-      .expect('Content-Type', /json/)
+      .get(API_FIND_WITHDRAW_REQUESTS_URL)
+      .set('Cookie', [token])
       .expect(200)
-      .end((err, res) => {
-        if (err) return done(err);
-        const token = res.headers['set-cookie'][0].split(';')[0];
-        expect(res).toBeDefined();
-        expect(res.body.email).toBe(loginData.email);
-        return request(app)
-          .get('/api/withdraw-requests')
-          .set('Cookie', [token])
-          .expect(200)
-          .expect('Content-Type', /json/)
-          .end(async (error, response) => {
-            if (error) return done(error);
-            expect(response.body).toBeDefined();
-            expect(Array.isArray(response.body)).toBe(true);
-            expect(response.body[0].user.name).toBeDefined();
-            return done();
-          });
+      .expect('Content-Type', /json/)
+      .end(async (error, response) => {
+        if (error) return done(error);
+        expect(response.body).toBeDefined();
+        expect(Array.isArray(response.body)).toBe(true);
+        expect(response.body[0].user.name).toBe(hostUser.name);
+        expect(response.body[0].status).toBe(pendingWithdrawRequest.status);
+        expect(response.body[0].amount).toBe(pendingWithdrawRequest.amount);
+        expect(response.body[0].bankName).toBe(pendingWithdrawRequest.bankName);
+        expect(response.body[0].bankSortCode).toBe(
+          pendingWithdrawRequest.bankSortCode,
+        );
+        expect(response.body[0].accountNumber).toBe(
+          pendingWithdrawRequest.accountNumber,
+        );
+
+        return done();
       });
-  }, 30000);
+  });
 
   test('Other users are unable to view all withdrawal requests', async done => {
-    const admin = await User.findOne({ role: 'host' });
-    const loginData = {
-      email: admin.email,
-      password: '123456',
-    };
-    // login as admin
+    const { hostUser } = users;
+
+    const token = `token=${createToken(hostUser._id)}`;
+
     request(app)
-      .post('/api/user/login')
-      .send(loginData)
+      .get(API_FIND_WITHDRAW_REQUESTS_URL)
+      .set('Cookie', [token])
+      .expect(403)
       .expect('Content-Type', /json/)
-      .expect(200)
-      .end((err, res) => {
-        if (err) return done(err);
-        const token = res.headers['set-cookie'][0].split(';')[0];
-        expect(res).toBeDefined();
-        expect(res.body.email).toBe(loginData.email);
-        return request(app)
-          .get('/api/withdraw-requests')
-          .set('Cookie', [token])
-          .expect(403)
-          .expect('Content-Type', /json/)
-          .end(async error => {
-            if (error) return done(error);
-            return done();
-          });
+      .end(async error => {
+        if (error) return done(error);
+        return done();
       });
-  }, 30000);
+  });
 });
